983153690d41b0b940bb8127926ea6d87e84c7ff
VeraTsitavets <tsitavetsva@gmail.com>
2025-06-14 15:52:28 +0300

    HW7

--- src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
+++ src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
@@ -49,7 +49,7 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
                       id={id + '-input-' + o.id}
                       className={finalRadioClassName}
                       type={'radio'}
-                      name={o.name}
+                      name={name}
                       checked={o.id === Number(value)}
                       value={o.id}
                       // name, checked, value делают студенты
