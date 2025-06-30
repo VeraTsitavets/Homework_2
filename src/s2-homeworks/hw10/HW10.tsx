f080c737f30f877eb60642cfc7de16165eeb129f
VeraTsitavets <tsitavetsva@gmail.com>
2025-06-30 14:27:11 +0300

    HW10

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -21,7 +21,7 @@ const HW10 = () => {
 
     const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
         // dispatch
-
+        dispatch(loadingAC(true))
         setTimeout(() => {
             dispatch(loadingAC(false))
         }, 1500)
