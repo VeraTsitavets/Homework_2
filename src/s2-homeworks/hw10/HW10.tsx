aa2f48812905c80fac326ee0847575620ed7f420
VeraTsitavets <tsitavetsva@gmail.com>
2025-06-30 14:36:28 +0300

    HW10

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -15,17 +15,18 @@ import {Loader} from './Loader'
 
 const HW10 = () => {
     // useSelector, useDispatch // пишет студент
-    const selector = useSelector<AppStoreType, boolean>(state => state.loading.isLoading)
+
     const dispatch = useDispatch()
-    const isLoading = false
+    const isLoading = useSelector<AppStoreType, boolean>(state => state.loading.isLoading)
 
     const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
         // dispatch
         dispatch(loadingAC(true))
+
+        // setTimeout
         setTimeout(() => {
             dispatch(loadingAC(false))
         }, 1500)
-        // setTimeout
 
     }
 
