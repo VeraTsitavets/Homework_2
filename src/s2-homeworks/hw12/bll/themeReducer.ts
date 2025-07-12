1020a031e6f1ed1a7520b5b8ad7af0693cec1576
VeraTsitavets <tsitavetsva@gmail.com>
2025-07-12 15:28:57 +0300

    HW12

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,25 @@
+import {AppStoreType} from "../../hw10/bll/store";
+
 const initState = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export const themeReducer = (state = initState, action: ChangeThemeActionType): typeof initState => { // fix any
     switch (action.type) {
         // дописать
+            case 'SET_THEME_ID':
+                return {
+                    ...state, themeId: action.id,
+                }
 
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+type ChangeThemeActionType = {
+    type: 'SET_THEME_ID'
+    id: number
+}
+
+export const changeThemeId = (id: number): ChangeThemeActionType => ({ type: 'SET_THEME_ID', id }) // fix any
