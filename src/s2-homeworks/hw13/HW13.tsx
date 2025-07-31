074fcaeaf5716d101c02172a1c5340be7da0d5b4
VeraTsitavets <tsitavetsva@gmail.com>
2025-07-31 14:42:27 +0300

    HW13

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -37,7 +37,7 @@ const HW13 = () => {
                 setCode('Код 200!')
                 setImage(success200)
                 setText('...всё ок)')
-                setInfo('')
+                setInfo('код 200 - обычно означает что скорее всего всё ок)')
                 // дописать
 
             })
@@ -48,18 +48,22 @@ const HW13 = () => {
                     setCode('Ошибка 500!')
                     setImage(error500)
                     setText('эмитация ошибки на сервере')
+                    setInfo('ошибка 500 - обычно означает что что-то сломалось на сервере, например база данных)')
+
                 } else if(e.response.status === 400) {
 
                     setCode('Ошибка 400!')
                     setImage(error400)
                     setText('Ты не отправил success в body вообще!' )
+                    setInfo('ошибка 400 - обычно означает что скорее всего фронт отправил что-то не то на бэк!')
 
                 } else {
                     setCode('Error!')
                     setImage(errorUnknown)
                     setText('Error' )
+                    setInfo('Error')
                 }
-                setInfo('')
+
             })
     }
 
